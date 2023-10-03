864b85b2a5f3e467faaa0342321c7ea55320c9ea
Derrick <derwee@gmail.com>
2023-10-03 23:24:04 +0200

    Points now look much better

--- src/wiggle_scale.gd
+++ src/wiggle_scale.gd
@@ -1,9 +1,9 @@
 extends Node2D
 
 var time := 0.0
-onready var orig_scale : Vector2 = scale
-export var power := 0.0
-export var speed := 1.0
+@onready var orig_scale : Vector2 = scale
+@export var power := 0.0
+@export var speed := 1.0
 
 func _process(delta):
 	time = wrapf(time+delta*speed, -PI,PI)
